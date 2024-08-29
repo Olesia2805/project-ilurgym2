ce782dba84b54f8000335f12077689840138677d
Olesia2805 <vysotina2002@gmail.com>
2024-08-29 23:38:38 +0200

    submit form

---
+++ src/js/footer.js
@@ -0,0 +1,47 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const formData = { email: '' };
+const formEl = document.querySelector('.subscribe-form');
+
+const isEmpty = form => {
+  const formFromLS = JSON.parse(localStorage.getItem('subscribe-form-email'));
+  if (formFromLS !== null) {
+    for (const key in formFromLS) {
+      if (formFromLS.hasOwnProperty(key)) {
+        form.elements[key].value = formFromLS[key];
+        formData[key] = formFromLS[key];
+      }
+    }
+  }
+};
+
+isEmpty(formEl);
+
+formEl.addEventListener('input', event => {
+  const nameField = event.target.name;
+  const valueField = event.target.value.trim();
+
+  formData[nameField] = valueField;
+
+  localStorage.setItem('subscribe-form-email', JSON.stringify(formData));
+});
+
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+  if (formData['email']) {
+    iziToast.success({
+      color: '#f4f4f4',
+      title: 'Congratulations!',
+      message: 'You have subscribed ✅',
+      position: 'bottomRight',
+      timeout: 5000,
+    });
+
+    localStorage.removeItem('subscribe-form-email');
+    event.target.reset();
+    formData.email = '';
+  } else {
+    alert('Fill please all fields');
+  }
+});
--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import './js/exercises.js';
 import './js/quote.js';
 import './js/modal.js';
 import './js/footer.js';
-import './js/paginations.js'
+import './js/paginations.js';
 import './js/mobile-menu.js';
