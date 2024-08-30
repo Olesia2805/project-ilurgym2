763d8129f2ab68c3d5565208c61a3d3107533832
Olesia2805 <vysotina2002@gmail.com>
2024-08-30 12:27:23 +0200

    subscribe

--- src/js/services/subscribe.js
+++ src/js/services/subscribe.js
@@ -2,9 +2,9 @@ import { showIziToast } from './iziToast';
 
 import { fetchApi } from './api-service';
 
-const form = document.getElementById('subscribe-form');
+const formEl = document.querySelector('.subscribe-form');
 
-form.addEventListener('submit', handleSubmit);
+formEl.addEventListener('submit', handleSubmit);
 
 async function handleSubmit(e) {
   e.preventDefault();
@@ -12,10 +12,36 @@ async function handleSubmit(e) {
 
   try {
     await fetchApi.addSubscription({ email });
-    showIziToast("Congrats! You've been subscribed!", '#53e276');
+    showIziToast('You have subscribed 🥳', '#f4f4f4', 'Congratulations!');
   } catch (err) {
-    showIziToast('Subscription already exists', '#FF876C');
+    showIziToast('Subscription already exists 😊', '#c6cdd1');
   } finally {
     e.target.reset();
+    localStorage.removeItem('subscribe-form-email');
   }
 }
+
+const formData = { email: '' };
+
+const isEmpty = form => {
+  const formFromLS = JSON.parse(localStorage.getItem('subscribe-form-email'));
+  if (formFromLS !== null) {
+    for (const key in formFromLS) {
+      if (formFromLS.hasOwnProperty(key)) {
+        form.elements[key].value = formFromLS[key];
+        formData[key] = formFromLS[key];
+      }
+    }
+  }
+};
+
+isEmpty(formEl);
+
+formEl.addEventListener('input', event => {
+  const nameField = event.target.name;
+  const valueField = event.target.value.trim();
+
+  formData[nameField] = valueField;
+
+  localStorage.setItem('subscribe-form-email', JSON.stringify(formData));
+});
